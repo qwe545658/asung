e8a3d54e649e20998b927c44b888cb019ac844f7
123 <karamischeff.ruslan@yandex.ru>
2024-12-24 16:04:05 +0700

    24

--- script.js
+++ script.js
@@ -3,22 +3,38 @@ const navButtom=document.querySelector(".header-2_nav-list-cater")
 const home=document.querySelector(".home");
 const women=document.querySelector(".women");
 const men=document.querySelector(".men");
-const electro=document.querySelector(".elecro");
-const lifestyle=document.querySelector(".lifestyle")
+const electro=document.querySelector(".electro");
+const lifestyle=document.querySelector(".jewelery")
+const down=document.querySelector(".section1_download")
 
 
+mass=[];
+massMen=[];
+massWomen=[];
+massElecro=[]
+massLife=[]
 
 console.log(cards)
 
 
+down.addEventListener("click",()=>{
+   let num1=6
+  let num2=12;
+  
+  
+
+  
+}
+)
+
 
 async function add() {
   try {
-    const response = await fetch("https://fakestoreapi.com/products");
+    const response = await fetch("https://fakestoreapi.com/products?limit=6");
     if (!response.ok) throw new Error("Ошибка загрузки");
     const data = await response.json();
     console.log(data)
-
+     
     diplayProduct(data);
   } catch (error) {
     cards.innerHTML = `<p>${error.message}</p>`;
@@ -26,12 +42,15 @@ async function add() {
 }
 
 function diplayProduct(products) {
-  products.forEach(element => {
-    const card = document.createElement("article");
+         
+   products.forEach((element) => {
+    
+
+      const card = document.createElement("article");
     card.classList.add('section1_cards_card')
     const Delete = document.createElement("button");
     card.innerHTML = `
-            <img src="${element.image}" alt="${element.title}" width='auto' height='300px'>
+            <img loading="lazy" src="${element.image}" alt="${element.title}" width='auto' height='300px'>
             <h3>${element.title}</h3>  
             <p>price ${element.price}</p>
             <p>rate ${element.rating.rate}</p>
@@ -45,8 +64,11 @@ function diplayProduct(products) {
 
     card.appendChild(Delete);
     cards.appendChild(card);
-  });
-}
+  })};
+      
+  
+    
+
 
 function deletePriduct(id) {
   fetch(`https://fakestoreapi.com/products/${id}`, {
@@ -56,21 +78,27 @@ function deletePriduct(id) {
     .then(data => {
       alert(`Товар удалён: ${data.title}`);
       
-    });
+    })
+    .catch(error=>
+      alert(error)
+    )
 }
 
 add()
 
 
 home.addEventListener("click",()=>{
-  navButtom.classList.remove("on");
-  home.classList.add("on");
-  window,location.href="index.html"
+  
+ 
+  window.location.href="index.html"
 })
 
 women.addEventListener("click",()=>{
-  navButtom.classList.remove("on");
   women.classList.add("on")
+  home.classList.remove("on");
+   men.classList.remove("on");
+   electro.classList.remove("on");
+   lifestyle.classList.remove("on");
   cards.innerHTML=""
   fetch("https://fakestoreapi.com/products/category/women's clothing")
     .then(response=>response.json())
@@ -78,5 +106,58 @@ women.addEventListener("click",()=>{
       console.log(data)
       diplayProduct(data)
     })
+    .catch(error=>
+      alert(error)
+    )
 
+})
+
+
+men.addEventListener("click",()=>{
+  men.classList.add("on");
+   home.classList.remove("on");
+   women.classList.remove("on");
+   electro.classList.remove("on");
+   lifestyle.classList.remove("on");
+
+  cards.innerHTML=""
+  fetch("https://fakestoreapi.com/products/category/men's clothing")
+  .then(response=>response.json())
+  .then(data=>{
+    console.log(data);
+    massMen=data
+    diplayProduct(massMen)
+  })
+  .catch(error=>
+    alert(error)
+  )
+})
+
+electro.addEventListener("click",()=>{
+  electro.classList.add("on")
+  home.classList.remove("on");
+   women.classList.remove("on");
+   men.classList.remove("on")
+   lifestyle.classList.remove("on");
+   cards.innerHTML=""
+   fetch("https://fakestoreapi.com/products/category/electronics")
+   .then(response=>response.json())
+   .then(data=>{
+    console.log(data)
+    diplayProduct(data)
+   }).catch(error=>alert(error))
+})
+
+lifestyle.addEventListener("click",()=>{
+  lifestyle.classList.add("on")
+  women.classList.remove("on")
+  men.classList.remove("on")
+  electro.classList.remove("on")
+  fetch("https://fakestoreapi.com/products/category/jewelery")
+   .then(response=>response.json())
+   .then(data=>{
+    console.log(data)
+    cards.innerHTML=""
+    diplayProduct(data)
+   }).catch(error=>alert(error))
 })
